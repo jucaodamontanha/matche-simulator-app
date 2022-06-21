c03095c05b9d9d5a0a0b2a22efdc09aa6bab1e93
jucao <jucaodamontanha@gmail.com>
2022-06-21 09:04:39 -0300

    introduzindo findById e as implementation

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -2,10 +2,22 @@ package me.dio.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
+import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+       // val tvHello = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater)
+
+
     }
 }
