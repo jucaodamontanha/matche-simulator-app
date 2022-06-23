c19e108802634013ccbd43087d6980cd2c2c3c2c
jucao <jucaodamontanha@gmail.com>
2022-06-23 12:30:35 -0300

    Desenvolvimento dos Layouts de listagem  e detalhes de partidas

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -1,9 +1,7 @@
 package me.dio.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // setContentView(R.layout.activity_main)
-       // val tvHello = findViewById<TextView>(R.id.tvHello)
 
-        binding = ActivityMainBinding.inflate(LayoutInflater)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
